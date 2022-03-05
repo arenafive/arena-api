29ac49d1cfda10c9e908b163775c98ea2ce7dfcc
amadou sy <amsada93@gmail.com>
2022-03-05 01:36:46 +0100

    setting environnent vartibales

--- core/settings.py
+++ core/settings.py
@@ -9,13 +9,20 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
+import os, json
+import environ
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Environnement variables
+env = environ.Env(
+        ALLOWED_HOSTS=(list, ["*",]),
+)
+
+env.read_env(os.path.join(BASE_DIR, '.env'), overwrite=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +33,8 @@ SECRET_KEY = 'django-insecure-(w^-!6)89)aq^l7@*dv8x=nn$m$@_r-qov^o3ae0mvi)7i-ex&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = json.loads(os.getenv('ALLOWED_HOSTS', env('ALLOWED_HOSTS')))
 
 
 # Application definition
