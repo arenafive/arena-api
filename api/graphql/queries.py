2009c81b4c31e6f5d0e36f359fb30a1c63580472
amadou sy <amsada93@gmail.com>
2022-08-22 23:47:23 +0200

    fix Field relay with token invalid

--- api/graphql/queries.py
+++ api/graphql/queries.py
@@ -15,37 +15,33 @@ from api.models import ArenaFiveSettings
 
 
 class GameQuery(ObjectType):
-    game = graphene.relay.Node.Field(GameNode, token=graphene.String(required=True))
+    game = graphene.relay.Node.Field(GameNode)
     games = DjangoFilterConnectionField(GameNode, token=graphene.String(required=True))
 
 
 class PlayerQuery(ObjectType):
-    player = graphene.relay.Node.Field(PlayerNode, token=graphene.String(required=True))
+    player = graphene.relay.Node.Field(PlayerNode)
     players = DjangoFilterConnectionField(
         PlayerNode, token=graphene.String(required=True)
     )
 
 
 class ManagerQuery(ObjectType):
-    manager = graphene.relay.Node.Field(
-        ManagerNode, token=graphene.String(required=True)
-    )
+    manager = graphene.relay.Node.Field(ManagerNode)
     managers = DjangoFilterConnectionField(
         ManagerNode, token=graphene.String(required=True)
     )
 
 
 class ArenaQuery(ObjectType):
-    arena = graphene.relay.Node.Field(ArenaNode, token=graphene.String(required=True))
+    arena = graphene.relay.Node.Field(ArenaNode)
     arenas = DjangoFilterConnectionField(
         ArenaNode, token=graphene.String(required=True)
     )
 
 
 class ArenaFiveSettingsQuery(ObjectType):
-    settings = graphene.Field(
-        ArenaFiveSettingsNode, token=graphene.String(required=True)
-    )
+    settings = graphene.Field(ArenaFiveSettingsNode)
 
     @login_required
     def resolve_settings(self, info):
