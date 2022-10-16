e3c0b96dbf67983cc61aabeff45390e59dd07297
amadou sy <amsada93@gmail.com>
2022-10-16 02:52:17 +0200

    fix migrations 5

--- api/migrations/0014_auto_20221009_0036.py
+++ api/migrations/0014_auto_20221009_0036.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("api", "0013_arenafivesettings"),
+        ("api", "0014_auto_20220908_2004"),
     ]
 
     operations = [
