47e6a5b4f8cd8defa41f912d9fee69f580c6afaa
amadou sy <amsada93@gmail.com>
2022-10-22 03:17:48 +0200

    push notification done

--- api/graphql/mutations.py
+++ api/graphql/mutations.py
@@ -298,13 +298,13 @@ class JoinGame(ClientIDMutation):
                 "to": game.captain.android_exponent_push_token,
                 "title": game.captain.full_name,
                 "body": f"a rejoint votre match du {game.start_date} ",
-                "data": {"key": "Game", "obj": {"game": game}},
+                "data": {"key": "Game", "obj": {"game": json.dumps(game)}},
             }
             data2 = {
                 "to": game.captain.ios_exponent_push_token,
                 "title": game.captain.full_name,
                 "body": f"a rejoint votre match du {game.start_date} ",
-                "data": {"key": "Game", "obj": {"game": game}},
+                "data": {"key": "Game", "obj": {"game": json.dumps(game)}},
             }
             logger.info(f"Sending push notification for android with payload({data1})")
             res = requests.post(
